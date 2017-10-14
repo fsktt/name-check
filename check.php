f8ec628bf988f102f279daeb8a3cc3afe99841c2
not-nil <tyler@niil.me>
2017-10-14 13:03:28 -0700

    bug fixes II

--- check.php
+++ check.php
@@ -3,10 +3,12 @@
 	if ($words) {
 		while (($line = fgets($words)) !== false) {
 			$filtered = str_replace("\n", "", $line);
-			$res = file_get_contents("http://www.roblox.com/UserCheck/DoesUsernameExist?username=" . $filtered);
+			if (strlen($filtered) >= 3) {
+				$res = file_get_contents("http://www.roblox.com/UserCheck/DoesUsernameExist?username=" . $filtered);
 
-			if ($res == '{"success":false}') {
-				file_put_contents("available.txt", $filtered . "\n", FILE_APPEND);
+				if ($res == '{"success":false}') {
+					file_put_contents("available.txt", $filtered . "\n", FILE_APPEND);
+				}
 			}
 		}
 
