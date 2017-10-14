d063107efd5385caa557f93121e079ccb2440636
not-nil <tyler@niil.me>
2017-10-14 12:51:41 -0700

    bug fixes

--- check.php
+++ check.php
@@ -2,13 +2,16 @@
 	$words = fopen("words_alpha.txt", "r");
 	if ($words) {
 		while (($line = fgets($words)) !== false) {
-			$res = file_get_contents("http://www.roblox.com/UserCheck/DoesUsernameExist?username=" . $line);
+			$filtered = str_replace("\n", "", $line);
+			$res = file_get_contents("http://www.roblox.com/UserCheck/DoesUsernameExist?username=" . $filtered);
+
 			if ($res == '{"success":false}') {
-				file_put_contents("available.txt", $line . "\n", FILE_APPEND);
+				file_put_contents("available.txt", $filtered, FILE_APPEND);
 			}
 		}
 
 		fclose($words);
+		echo "finished";
 	} else {
 		error("error opening wordlist");
 	}
