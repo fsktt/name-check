917e892f8ae4d075c4b0d2ef3e95717cb1ea2c35
fsktt <tyler@niil.me>
2018-01-14 20:09:20 -0700

    Update check.php

--- check.php
+++ check.php
@@ -1,10 +1,11 @@
 <?php
+	$api_url = "";
 	$words = fopen("words_alpha.txt", "r");
 	if ($words) {
 		while (($line = fgets($words)) !== false) {
 			$filtered = str_replace("\n", "", $line);
 			if (strlen($filtered) >= 3) {
-				$res = file_get_contents("http://www.roblox.com/UserCheck/DoesUsernameExist?username=" . $filtered);
+				$res = file_get_contents($api_url . $filtered);
 
 				if ($res == '{"success":false}') {
 					file_put_contents("available.txt", $filtered . "\n", FILE_APPEND);
